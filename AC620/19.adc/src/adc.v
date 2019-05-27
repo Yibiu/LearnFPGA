b54ac5e2abbd7392b05d0aea73124bab6e0c79f0
yibu_li <2497388365@qq.com>
2019-05-27 20:00:29 +0800

    add 19.adc

---
+++ AC620/19.adc/src/adc.v
@@ -0,0 +1,22 @@
+/**
+* @brief:
+* ADC-128s022
+*/
+module adc(
+	input wire clk_50mhz,
+	input wire rst_n,
+	input wire en,
+	input wire [2:0] channel,
+	input wire [7:0] div_param,
+	input wire data,
+	output reg [11:0] adc_data,
+	output reg adc_done,
+	output reg adc_state,
+	output reg adc_sclk,
+	output reg adc_din,
+	
+);
+
+
+endmodule
+
