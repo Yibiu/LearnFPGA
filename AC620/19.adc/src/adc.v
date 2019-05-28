361efc81a466b321a8507f95ff29049cd1b0ce13
yibu_li <2497388365@qq.com>
2019-05-28 19:21:50 +0800

    19.adc

--- AC620/19.adc/src/adc.v
+++ AC620/19.adc/src/adc.v
@@ -1,6 +1,7 @@
 /**
 * @brief:
-* ADC-128s022
+* ADC-128S022
+* SCLK - 0.8~3.2MHz
 */
 module adc(
 	input wire clk_50mhz,
@@ -9,14 +10,91 @@ module adc(
 	input wire [2:0] channel,
 	input wire [7:0] div_param,
 	input wire data,
-	output reg [11:0] adc_data,
-	output reg adc_done,
 	output reg adc_state,
-	output reg adc_cs_n,
+	output wire adc_cs_n,
 	output reg adc_sclk,
-	output reg adc_din // DIN(addr)
+	output reg adc_din, // addr
+	output reg [11:0] adc_data,
+	output reg adc_done
 );
 
+reg [5:0] state_cnt;
+
+// State(start/stop)
+always @(posedge clk_50mhz or negedge rst_n)
+	if (rst_n == 1'b0)
+		adc_state <= 1'b0;
+	else if (en)
+		adc_state <= 1'b1;
+	else if (state_cnt == 6'd32)
+		adc_state <= 1'b0;
+	else
+		adc_state <= adc_state;
+		
+assign adc_cs_n = ~adc_state;
+
+// 50MHz --> 50MHz / div_param
+// 状态计数
+reg [7:0] cnt;
+always @(posedge clk_50mhz or negedge rst_n)
+	if (rst_n == 1'b0)
+		cnt <= 8'd0;
+	else if (adc_state) begin
+		if (cnt == (div_param - 1'b1))
+			cnt <= 8'd0;
+		else
+			cnt <= cnt + 1'b1;
+	end
+	else
+		cnt <= 8'd0;
+		
+always @(posedge clk_50mhz or negedge rst_n)
+	if (rst_n == 1'b0)
+		state_cnt <= 6'd0;
+	else if (adc_state) begin
+		if (cnt == (div_param - 1'b1)) begin
+			if (state_cnt == 6'd32)
+				state_cnt <= 6'd0;
+			else
+				state_cnt <= state_cnt + 1'b1;
+		end
+		else
+			state_cnt <= state_cnt;
+	end
+	else
+		state_cnt <= 6'd0;
+		
+// 状态迁移
+always @(posedge clk_50mhz or negedge rst_n)
+	if (rst_n == 1'b0) begin
+		adc_sclk <= 1'b1;
+		adc_din <= 1'b1;
+		adc_data <= 12'd0;
+		adc_done <= 1'b0;
+	end
+	else if (adc_state)
+		case(state_cnt)
+			6'd0:begin adc_sclk<=1'b0; adc_din<=1'b0;adc_data<=12'd0;adc_done<=1'b0;end
+			6'd1:begin adc_sclk<=1'b1;end
+			6'd2:begin adc_sclk<=1'b0;end
+			6'd3:begin adc_sclk<=1'b1;end
+			6'd4:begin adc_sclk<=1'b0;adc_din<=channel[2];end
+			6'd5:begin adc_sclk<=1'b1;end
+			6'd6:begin adc_sclk<=1'b0;adc_din<=channel[1];end
+			6'd7:begin adc_sclk<=1'b1;end
+			6'd8:begin adc_sclk<= 1'b0; adc_din<=channel[0];end
+			6'd9,6'd10,6'd12,6'd14,6'd16,6'd18,6'd20,6'd22,6'd24,6'd26,6'd28,6'd30:
+				begin adc_sclk <= 1'b1;adc_data <= {adc_data[10:0], data};end
+			6'd11,6'd13,6'd15,6'd17,6'd19,6'd21,6'd23,6'd25,6'd27,6'd29,6'd31:
+				begin adc_sclk <= 1'b0;end
+			6'd32:begin adc_sclk<=1'b1;adc_done<=1'b1;end
+			default:;
+		endcase
+	else begin
+		adc_sclk <= 1'b1;
+		adc_din <= 1'b1;
+		adc_data <= 12'd0;
+		adc_done <= 1'b0;
+	end
 
 endmodule
-
---
+++ AC620/19.adc/tb/tb_adc.v
@@ -0,0 +1,67 @@
+`timescale 1ns/1ns
+
+
+module tb_adc(
+);
+
+reg tb_clk_50mhz;
+reg tb_rst_n;
+reg tb_en;
+reg [2:0] tb_channel;
+reg [7:0] tb_div_param;
+reg tb_data;
+wire tb_adc_state;
+wire tb_adc_cs_n;
+wire tb_adc_sclk;
+wire tb_adc_din;
+wire [11:0] tb_adc_data;
+wire tb_adc_done;
+
+parameter CLK_NS = 20;
+
+// 例化
+module adc(
+	.clk_50mhz(tb_clk_50mhz),
+	.rst_n(tb_rst_n),
+	.en(tb_en),
+	.channel(tb_channel),
+	.div_param(tb_div_param),
+	.data(tb_data),
+	.adc_state(tb_adc_state),
+	. adc_cs_n(tb_adc_cs_n),
+	.adc_sclk(tb_adc_sclk),
+	.adc_din(tb_adc_din),
+	.adc_data(tb_adc_data),
+	.adc_done(tb_adc_done)
+);
+
+// 时钟
+always #(CLK_NS / 2) tb_clk_50mhz = ~tb_clk_50mhz;
+
+// 初始化
+initial begin
+	tb_clk_50mhz = 1'b0;
+	tb_rst_n = 1'b0;
+	tb_en = 1'b0;
+	tb_channel = 3'd0;
+	tb_div_param = 8'd0;
+	tb_data = 1'b0;
+	#(CLK_NS * 20)
+	
+	tb_channel = 3'd5;
+	tb_div_param = 8'd13;
+	tb_data = 1'b1;
+	tb_rst_n = 1'b1;
+	#(CLK_NS * 20)
+	
+	tb_en = 1'b1;
+	#(CLK_NS);
+	
+	//wait(tb_adc_done);
+	#(CLK_NS * 1000)
+	
+	$stop;
+end
+
+endmodule
+
